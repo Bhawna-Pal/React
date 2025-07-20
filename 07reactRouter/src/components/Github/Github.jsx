2a1ee7d207b42da95593d4f08a8ded4ca727cb53
Bhawna Pal <bhawnapal2104@gmail.com>
2025-07-20 22:55:41 +0530

    add Github followers

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from 'react'
 import { data } from 'react-router-dom'
+import { useLoaderData } from 'react-router-dom'
 
 function Github() {
-   const [data, setData] = useState([])
+  const data = useLoaderData()
+  //  const [data, setData] = useState([])
 
-    useEffect(() => {
-        fetch('https://api.github.com/users/Bhawna-Pal')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            setData(data)
-        })
-    }, [])
+  //   useEffect(() => {
+  //       fetch('https://api.github.com/users/Bhawna-Pal')
+  //       .then(response => response.json())
+  //       .then(data => {
+  //           console.log(data);
+  //           setData(data)
+  //       })
+  //   }, [])
   return (
     <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>
       Github followers: {data.followers}
@@ -21,3 +23,8 @@ function Github() {
 }
 
 export default Github
+
+export const githubInfoLoader = async () => {
+ const response = await fetch('https://api.github.com/users/Bhawna-Pal')
+ return response.json()
+}
