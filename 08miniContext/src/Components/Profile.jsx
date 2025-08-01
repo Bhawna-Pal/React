d6b6d523fc29ebb332a2c6b82261497c79ac504a
Bhawna Pal <bhawnapal2104@gmail.com>
2025-08-01 23:28:24 +0530

    Context Api

--- 08miniContext/src/Components/Profile.jsx
+++ 08miniContext/src/Components/Profile.jsx
@@ -2,10 +2,12 @@ import React, {useContext} from 'react'
 import UserContext from '../Context/UserContext'
 
 function Profile() {
+
     const {user} = useContext(UserContext)
+    
     if(!user) return <div>Please Login</div>
 
-    return <div>WelCome {user.username}</div>
+    return <div>Welcome {user.username}</div>
 }
 
 export default Profile
